416d6e5b82029318b89c1f4d43fea2b00fbbc14e
plabhesh98 <81747071+plabhesh98@users.noreply.github.com>
2021-11-15 14:54:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+ var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function run(event) {
+    console.log(event);
+    var Content = event.results [0] [0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML = Content;
+    speak()
+}
+
+function speak () {
+    var synth = window.speechSynthesis; 
+    speak_data = document.getElementById("textbox").innerHTML;
+    var utterThis = new SpeechSynthesisUtterance(speak_data); 
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width: 320,
+    height: 240,
+    image_format: 'jpeg',
+    jpeg_quality: 10
+});
+camera = document.getElementById("camera");
